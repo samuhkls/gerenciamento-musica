6a7f013c46a12a1efe149e2fbb5871f30365fbaf
samuhkls <samuel.henriquess21@gmail.com>
2024-04-29 20:19:45 -0300

    integrado com banco

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,27 +4,10 @@ import service.SearchTrack;
 
 public class Main {
     public static void main(String[] args) {
-        RequestTrack request = new RequestTrack();
-        SearchTrack pesquisa = new SearchTrack();
 
-        request.pegarInfo();
-        pesquisa.pesquisar();
 
-        System.out.println("-------");
 
-        setterMusica setter = new setterMusica(request);
 
-        setter.setarInfo();
-
-        Musica musica = setter.getMusica();
-        System.out.println("nome da musica: " + musica.getNome());
-        System.out.println("nome do artista: " + musica.getArtista());
-        System.out.println("duracao da musica: " + musica.getDuracao());
-        System.out.println("----");
-        System.out.println(musica);
-
-        musicaDAO dao = new musicaDAO();
-        dao.createMusica(musica);
     }
 
 
--- src/main/java/servlet/CreateMusicaServlet.java
+++ src/main/java/servlet/CreateMusicaServlet.java
@@ -8,6 +8,7 @@ import model.Musica;
 import dao.musicaDAO;
 import org.json.JSONArray;
 import org.json.JSONObject;
+import service.SearchTrack;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -19,18 +20,19 @@ import java.io.IOException;
 @WebServlet("/create-musica")
 public class CreateMusicaServlet extends HttpServlet{
 
-        String trackNome;
-        String artistaNome;
 
-        double trackDuracao;
-        Musica musica = new Musica();
+
+        SearchTrack search = new SearchTrack();
 
         @Override
         protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
             String musicanome = request.getParameter("musica");
 
-            pesquisar(musicanome);
+            Musica musica = search.pesquisar(musicanome);
+
+            System.out.println("Nome da musica: " + musica.getNome());
+            System.out.println("Nome do artista: " + musica.getArtista());
 
             new musicaDAO().createMusica(musica);
 
@@ -38,52 +40,6 @@ public class CreateMusicaServlet extends HttpServlet{
 
         }
 
-        String API_KEY = "43fce9a2d0129c54d3c32cc47389254b";
-        String USER_AGENT = "ListenerApp";
-
-        String resposta;
-
-        @Override
-        public String toString() {
-            return "RequestTrack{} " + resposta ;
-        }
-
-        public void pesquisar(String musicaNome){
-            try{
-
-                HttpResponse<JsonNode> resposta = Unirest.get("https://ws.audioscrobbler.com/2.0/")
-                        .header("user-agent", USER_AGENT)
-                        .queryString("api_key", API_KEY) // parametros para a formação da url, antes de mandar a request
-                        .queryString("method", "track.search")
-                        .queryString("limit", "5")
-                        .queryString("track", musicaNome)
-                        .queryString("format", "json")
-                        .asJson();
-
-                JSONObject jsonResposta = new JSONObject(resposta.getBody());
-
-                System.out.println(jsonResposta);
-
-                // formatando a resposta json, que retorna um array com todos os resultados chamado "track"
-                JSONArray trackObject = jsonResposta.getJSONObject("object").getJSONObject("results").getJSONObject("trackmatches").getJSONArray("track");
-                JSONObject track = trackObject.getJSONObject(0); // acessando o primeiro index, que é o primeiro resultado
-
-                trackNome = track.getString("name");  // o nome é um atributo do objeto musica
-                System.out.println("Nome da musica: " + trackNome);
-
-                artistaNome = track.getString("artist"); // o nome do artista tambem é um atributo do objeto musica
-                System.out.println("Nome do artista: " + artistaNome);
-
-
-            }catch (UnirestException e) {
-                e.printStackTrace();
-            }
-        }
-
-    public void setarInfo(){
-        musica.setNome(trackNome);
-        musica.setArtista(artistaNome);
-    }
 
     }
 
